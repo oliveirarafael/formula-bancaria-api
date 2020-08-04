055e3ac41655caa559f7893aa0fbd80a929e6d7b
Rafael Rodrigues de Oliveira <oliveira.r.rafael@gmail.com>
2020-08-04 05:37:16 -0300

    Liberando endpoint esqueceu senha metodo get

--- src/main/java/br/com/formula/bancaria/api/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/formula/bancaria/api/config/security/SecurityConfigurations.java
@@ -48,7 +48,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers(HttpMethod.POST, "/auth", "/usuarios/alunos", "/usuarios/esqueceu-senha").permitAll()
+            .antMatchers(HttpMethod.POST, "/auth", "/usuarios/alunos").permitAll()
+            .antMatchers(HttpMethod.GET, "/usuarios/esqueceu-senha").permitAll()
             .anyRequest().authenticated()
             //.and().cors()
             .and().csrf().disable()
