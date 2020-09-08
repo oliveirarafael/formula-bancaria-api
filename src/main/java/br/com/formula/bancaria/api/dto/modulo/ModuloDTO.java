93cccc5c7a98bc47187120d4ed71efcaa703c023
Leonardo Sousa <leonardofsousa@outlook.com>
2020-09-08 02:03:13 -0300

    Ajustando metodo de listagem de modulos de um simulado

--- src/main/java/br/com/formula/bancaria/api/dto/modulo/ModuloDTO.java
+++ src/main/java/br/com/formula/bancaria/api/dto/modulo/ModuloDTO.java
@@ -6,19 +6,49 @@ import org.springframework.data.domain.Page;
 
 import br.com.formula.bancaria.api.model.entity.Modulo;
 
-public class ModuloDTO{
+public class ModuloDTO {
 
+    private Long id;
     private String uuid;
     private Integer quantidadeQuestoesPorSimulado;
     private LocalDateTime dataHoraCriacao;
+    private String nome;
+    private String descricao;
 
-    public ModuloDTO(Modulo modulo){
+    public ModuloDTO(Modulo modulo) {
+        this.id = modulo.getId();
         this.uuid = modulo.getUuid().toString();
         this.quantidadeQuestoesPorSimulado = modulo.getQuantidadeQuestoesPorSimulado();
         this.dataHoraCriacao = modulo.getDataHoraCriacao();
+        this.setNome(modulo.getNome());
+        this.setDescricao(modulo.getDescricao());
     }
 
-	public String getUuid() {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUuid() {
 		return uuid;
     }
     
